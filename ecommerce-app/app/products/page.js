f2d9690b512b10ede9adc73bcaf68f3080982d9e
mufeed kamal <mufeedkamal@gmail.com>
2023-12-16 12:11:34 -0600

    restarting without app router

--- ecommerce-app/app/products/page.js
+++
@@ -1,7 +0,0 @@
-import { initMongoose } from "../lib/mongoosey.js";
-import Product from "../models/product.js";
-
-export default async function handle(req, res) {
-    await initMongoose();
-    res.json(await Product.find().exec()) ;
-}
