0979f3153e2eea3f658f29f883647f6d2a3fd232
mufeed kamal <mufeedkamal@gmail.com>
2023-12-17 16:21:22 -0600

    changes on chekcout to pull data

--- keyboard-store/components/Product.js
+++ keyboard-store/components/Product.js
@@ -14,7 +14,7 @@ export default function Product({_id, name, price, description, picture}) {
             <div className="mt-2">
               <h3 className="font-bold text-lg">{name}</h3>
             </div>
-            <p className="text-sm mt-1 leading-4">{description}</p>
+            <p className="text-sm mt-1 leading-4 text-gray-500">{description}</p>
             <div className="flex mt-1">
               <div className="text-2xl font-bold grow">${price}</div>
               <button onClick={addProduct} className="bg-emerald-400 text-white py-1nb px-3 rounded-xl">+</button>
--- keyboard-store/pages/checkout.js
+++ keyboard-store/pages/checkout.js
@@ -1,9 +1,54 @@
 import Layout from "@/components/Layout";
+import { ProductsContext } from "@/components/ProductsContext";
+import { useContext, useEffect, useState } from "react";
 
 export default function Checkout() {
+    const {selectedProducts, setSelectedProducts} = useContext(ProductsContext)
+    const [productsInfo, setProductsInfo] = useState([]);
+    
+    useEffect(() => {
+    const uniqueIds = [...new Set(selectedProducts)];
+        fetch('/api/products?ids=' +uniqueIds.join(','))
+        .then(Response => Response.json()
+        .then(json => setProductsInfo(json)));
+    }, [selectedProducts]);
+
+    function moreOfThisProduct(id) {
+        setSelectedProducts(prev => [...prev,id])
+    }
+    function lessOfThisProduct(id) {
+
+    }
+
+
     return (
         <Layout>
-            checkout
+            {!productsInfo.length && (
+                <div>no products in your shopping cart</div>
+            )}
+            {productsInfo.length && productsInfo.map(productInfo => (
+                <div className="flex mb-5">
+                    <div className="bg-gray-100 p-3 rounded-xl shrink-0">
+                        <img className="w-24" src={productInfo.picture} alt=""></img>
+                    </div>
+                    <div className="pl-4">
+                        <h3 className="font-bold text-lg">{productInfo.name}</h3>
+                        <p className="text-sm leading-4 text-gray-500">{productInfo.description}</p>
+                        <div className="flex">
+                            <div className="grow">
+                                ${productInfo.price}
+                            </div>
+                            <div>
+                                <button className="border border-emerald-500 rounded-lg px-2 ">-</button>
+                                <span className="px-2">
+                                    {selectedProducts.filter(id => id === productInfo._id).length}
+                                </span>
+                                <button onClick={() => moreOfThisProduct(productInfo._id)} className="bg-emerald-500 rounded-lg px-2 text-white ">+</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </Layout>
     );
 }
