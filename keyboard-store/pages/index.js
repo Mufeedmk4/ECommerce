e92ab21a0fb86aecd9e6aefc85672473f8062f3f
mufeed kamal <mufeedkamal@gmail.com>
2023-12-16 18:15:59 -0600

    got rid of styles, changed tailwind hide scroll bar, changes in index

--- keyboard-store/pages/index.js
+++ keyboard-store/pages/index.js
@@ -6,6 +6,7 @@ import {useEffect, useState} from "react";
 
 export default function Home() {
   const [productsInfo, setProductsInfo] = useState([]);
+  const [phrase, setPhrase] = useState('');
   useEffect (() => {
     fetch('/api/products') 
     .then(response => response.json())
@@ -17,15 +18,18 @@ export default function Home() {
   console.log(categoriesNames)
   return (
     <div className="p-5">
+      <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search for products..." className="bg-gray-100 1-full py-2 px-4 rounded-xl"></input>
       <div>
         {categoriesNames.map(categoryName => (
           <div key={categoryName}>
-            <h2 className="text-2xl capitalize">{categoryName}</h2>
-            {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
-              <div>
-                <Product {...productInfo}/>
-              </div>
-            ))}
+            <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
+            <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+              {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
+                <div key={productInfo._id} className="px-5 snap-start">
+                  <Product {...productInfo}/>
+                </div>
+              ))}
+            </div>
           </div>
 
         ))}
