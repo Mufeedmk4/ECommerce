e90f6abc4f12d57e611d67ba0dae6aa510232569
mufeed kamal <mufeedkamal@gmail.com>
2023-12-17 21:42:53 -0600

    checkout page done

--- keyboard-store/pages/checkout.js
+++ keyboard-store/pages/checkout.js
@@ -5,7 +5,11 @@ import { useContext, useEffect, useState } from "react";
 export default function Checkout() {
     const {selectedProducts, setSelectedProducts} = useContext(ProductsContext)
     const [productsInfo, setProductsInfo] = useState([]);
-    
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
     useEffect(() => {
     const uniqueIds = [...new Set(selectedProducts)];
         fetch('/api/products?ids=' +uniqueIds.join(','))
@@ -17,9 +21,22 @@ export default function Checkout() {
         setSelectedProducts(prev => [...prev,id])
     }
     function lessOfThisProduct(id) {
-
+        const pos = selectedProducts.indexOf(id);
+        if (pos !== -1) {
+          setSelectedProducts( prev => {
+            return prev.filter((value,index) => index !== pos);
+          });
+        }
+      }
+    const deliveryPrice = 5;
+    let Subtotal = 0;
+    if (selectedProducts?.length) {
+        for (let id of selectedProducts){
+            const price = productsInfo.find(p => p._id === id).price;
+            Subtotal += price;
+        }
     }
-
+    const total = Subtotal + deliveryPrice
 
     return (
         <Layout>
@@ -39,7 +56,7 @@ export default function Checkout() {
                                 ${productInfo.price}
                             </div>
                             <div>
-                                <button className="border border-emerald-500 rounded-lg px-2 ">-</button>
+                                <button onClick={() => lessOfThisProduct(productInfo._id)} className="border border-emerald-500 rounded-lg px-2 ">-</button>
                                 <span className="px-2">
                                     {selectedProducts.filter(id => id === productInfo._id).length}
                                 </span>
@@ -48,7 +65,29 @@ export default function Checkout() {
                         </div>
                     </div>
                 </div>
+                
             ))}
+            <div className="mt-4">
+                <input onChange={e => setAddress(e.target.value)} value={address} className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"type="text" placeholder="Street address, number"/>
+                <input onChange={e => setCity(e.target.value)} value={city} className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"type="text" placeholder="City and postal code"/>
+                <input onChange={e => setName(e.target.value)} value={name} className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"type="text" placeholder="Your name"/>
+                <input onChange={e => setEmail(e.target.value)} value={email} className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"type="email" placeholder="Email Address"/>
+            </div>
+            <div className="mt-4">
+                <div className="flex my-3">
+                    <h3 className="grow font-bold text-gray-400">Subtotal:</h3>
+                    <h3 className="font-bold">${Subtotal}</h3>
+                </div>
+                <div className="flex my-3">
+                    <h3 className="grow font-bold text-gray-400">Delivery:</h3>
+                    <h3 className="font-bold">${deliveryPrice}</h3>
+                </div>
+                <div className="flex my-3 border-t-2 pt-3 border-emerald-400">
+                    <h3 className="grow font-bold text-gray-400">Total:</h3>
+                    <h3 className="font-bold">${total}</h3>
+                </div>
+            </div>
+            <button className="bg-emerald-500 py-2 px-5 rounded-xl font-bold text-white w-full my-4 shadow-emerald-300 shadow-lg">Pay ${total}</button>
         </Layout>
     );
 }
